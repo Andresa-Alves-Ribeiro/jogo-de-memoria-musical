32ce94075db9108acbc6462eba12fcbadd214437
Andresa <andresa_15ga@hotmail.com>
2024-03-12 11:57:33 -0300

    adicionando ordem aleatoria

--- src/data/familys.tsx
+++ src/data/familys.tsx
@@ -2,6 +2,7 @@ import violinoAudio from './audios/cordasDeArco/violino.wav';
 import violonceloAudio from './audios/cordasDeArco/violoncelo.wav';
 import violaAudio from './audios/cordasDeArco/viola-classica.wav';
 import contrabaixoAcusticoAudio from './audios/cordasDeArco/contrabaixo-acustico.wav';
+
 import banjoAudio from './audios/cordasDedilhadas/banjo.wav';
 import cavaquinhoAudio from './audios/cordasDedilhadas/cavaquinho.wav';
 import contrabaixoEletricoaAudio from './audios/cordasDedilhadas/contrabaixo-eletrico.wav';
@@ -10,11 +11,36 @@ import harpaAudio from './audios/cordasDedilhadas/harpa.wav';
 import ukeleleAudio from './audios/cordasDedilhadas/ukelele.wav';
 import violaCaipiraAudio from './audios/cordasDedilhadas/viola-caipira.wav';
 import violaoAudio from './audios/cordasDedilhadas/violao.wav';
+
 import tromboneAudio from './audios/metais/trombone.wav';
 import trompaAudio from './audios/metais/trompa.wav';
 import trompeteAudio from './audios/metais/trompete.wav';
 import tubaAudio from './audios/metais/tuba.wav';
+
 import fagoteAudio from "./audios/madeiras/fagote.wav"
+import clarineteAudio from "./audios/madeiras/clarinete.mp3"
+import flautaDoceAudio from "./audios/madeiras/flauta-doce.mp3"
+import flautaTransversalAudio from "./audios/madeiras/flauta-transversal.mp3"
+import oboeAudio from "./audios/madeiras/oboe.mp3"
+import saxofoneAudio from "./audios/madeiras/saxofone.mp3"
+
+import metalofoneAudio from "./audios/percussãoMelodica/metalofone.mp3"
+import sinosAudio from "./audios/percussãoMelodica/sinos.mp3"
+import tubosSonorosAudio from "./audios/percussãoMelodica/tubos-sonoros.mp3"
+import vibrafoneAudio from "./audios/percussãoMelodica/vibrafone.mp3"
+import xilofoneAudio from "./audios/percussãoMelodica/xilofone.mp3"
+
+import bateriaAudio from "./audios/percussãoRitmica/bateria.mp3"
+import pratosAudio from "./audios/percussãoRitmica/pratos.mp3"
+import timpanosAudio from "./audios/percussãoRitmica/timpanos.mp3"
+import trianguloAudio from "./audios/percussãoRitmica/triangulo.mp3"
+
+import escaletaAudio from "./audios/teclas/escaleta.mp3"
+import orgaoAudio from "./audios/teclas/orgao.mp3"
+import pianoAudio from "./audios/teclas/piano.mp3"
+import sanfonaAudio from "./audios/teclas/sanfona.mp3"
+import tecladoAudio from "./audios/teclas/teclado.mp3"
+
 
 export const instrumentFamilies: Array<string> = [
     "Cordas de arco",
@@ -50,31 +76,31 @@ export const instrumentsByFamily: { [key: string]: Array<{ id: number, name: str
         { id: 4, name: "Tuba", audio: tubaAudio }
     ],
     "Madeiras": [
-        { id: 1, name: "Flauta Doce", audio: violinoAudio }, //
-        { id: 2, name: "Flauta Transversal", audio: violinoAudio }, //
-        { id: 3, name: "Clarinete", audio: violinoAudio }, //
-        { id: 4, name: "Oboé", audio: violinoAudio }, //
+        { id: 1, name: "Flauta Doce", audio: flautaDoceAudio },
+        { id: 2, name: "Flauta Transversal", audio: flautaTransversalAudio },
+        { id: 3, name: "Clarinete", audio: clarineteAudio },
+        { id: 4, name: "Oboé", audio: oboeAudio },
         { id: 5, name: "Fagote", audio: fagoteAudio },
-        { id: 6, name: "Saxofone", audio: violinoAudio } //
-    ],
-    "PercussãoRitmica": [
-        { id: 1, name: "Tímpanos", audio: violinoAudio },
-        { id: 2, name: "Pratos", audio: violaAudio },
-        { id: 3, name: "Triângulo", audio: violonceloAudio },
-        { id: 4, name: "Bateria", audio: violinoAudio }
+        { id: 6, name: "Saxofone", audio: saxofoneAudio }
     ],
     "percussãoMelodica": [
-        { id: 1, name: "Metalofone", audio: violinoAudio },
-        { id: 2, name: "Xilofone", audio: violaAudio },
-        { id: 3, name: "Vibrafone", audio: violonceloAudio },
-        { id: 4, name: "Sinos", audio: violinoAudio },
-        { id: 5, name: "Tubos Sonoros", audio: violinoAudio }
+        { id: 1, name: "Metalofone", audio: metalofoneAudio },
+        { id: 2, name: "Xilofone", audio: xilofoneAudio },
+        { id: 3, name: "Vibrafone", audio: vibrafoneAudio },
+        { id: 4, name: "Sinos", audio: sinosAudio },
+        { id: 5, name: "Tubos Sonoros", audio: tubosSonorosAudio }
+    ],
+    "PercussãoRitmica": [
+        { id: 1, name: "Tímpanos", audio: timpanosAudio },
+        { id: 2, name: "Pratos", audio: pratosAudio },
+        { id: 3, name: "Triângulo", audio: trianguloAudio },
+        { id: 4, name: "Bateria", audio: bateriaAudio }
     ],
     "Teclas": [
-        { id: 1, name: "Piano", audio: violinoAudio },
-        { id: 2, name: "Teclado", audio: violaAudio },
-        { id: 3, name: "Órgão", audio: violonceloAudio },
-        { id: 4, name: "Sanfona", audio: violinoAudio },
-        { id: 5, name: "Escaleta", audio: violinoAudio }
+        { id: 1, name: "Piano", audio: pianoAudio },
+        { id: 2, name: "Teclado", audio: tecladoAudio },
+        { id: 3, name: "Órgão", audio: orgaoAudio },
+        { id: 4, name: "Sanfona", audio: sanfonaAudio },
+        { id: 5, name: "Escaleta", audio: escaletaAudio }
     ]
 };
--- src/pages/Game.tsx
+++ src/pages/Game.tsx
@@ -1,29 +1,45 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import FlippableCard from "../components/FlippableCard";
 import { instrumentsByFamily } from "../data/familys";
 
 export default function Game() {
     const [isFlipped, setIsFlipped] = useState(false);
+    const [cards, setCards] = useState<{ id: number; name: string; audio: string }[]>([]);
 
     const handleFlip = () => {
         setIsFlipped(!isFlipped);
     };
 
-    const cards = instrumentsByFamily["CordasDeArco"];
-    const multipleCards = cards.length * 2;
+    useEffect(() => {
+        const cardsCopy = [...instrumentsByFamily["CordasDedilhadas"]];
+        const lastCardId = cardsCopy[cardsCopy.length - 1].id;
+        const duplicatedCards = cardsCopy.map((card, index) => ({ ...card, id: index + lastCardId + 1 }));
+        const shuffledCards = shuffleArray([...cardsCopy, ...duplicatedCards]);
+        const shuffledAudio = shuffleArray(shuffledCards.map(card => card.audio));
+        const cardsWithAudio = shuffledCards.map((card, index) => ({ ...card, audio: shuffledAudio[index] }));
+        setCards(cardsWithAudio);
+    }, []);
+
+
+    const shuffleArray = (array: any[]) => {
+        for (let i = array.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [array[i], array[j]] = [array[j], array[i]];
+        }
+        return array;
+    };
 
     return (
         <div className='background h-screen flex items-center justify-center'>
-            <div style={{ backgroundColor: 'rgba(0, 0, 0, 0.8)' }} className='grid grid-cols-4 h-5/6 w-1/2 items-center pl-32 rounded-lg'>
-                {Array.from({ length: multipleCards }).map((_item, index) => {
-                    const audioPath = cards[index % cards.length].audio;
+            <div style={{ backgroundColor: 'rgba(0, 0, 0, 0.726)' }} className='grid grid-cols-4 h-5/6 w-1/2 items-center justify-center px-auto rounded-lg border-2 border-white'>
+                {cards.sort((a, b) => a.id - b.id).map((card, index) => {
                     return (
                         <FlippableCard
                             key={index}
                             isFlipped={isFlipped}
                             onClick={handleFlip}
-                            content={cards[index % cards.length].id}
-                            audio={audioPath}
+                            content={card.id}
+                            audio={card.audio}
                         />
                     );
                 })}
